2a589705f556b511c9a38f3c8ae018cbad944e4f
pcan_ <pcan_@LAPTOP-E5O1ECO4>
2023-03-26 15:38:45 +0300

    Proje dosyası ekle.

---
+++ gorselodev1/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace gorselodev1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttoncursor = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 41);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Kirmizi buton getir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 255);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(145, 40);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(233, 269);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(474, 174);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "buradaki yazi font size ve stil degisecek renk kirmizi olacak";
+            // 
+            // buttoncursor
+            // 
+            this.buttoncursor.Location = new System.Drawing.Point(735, 332);
+            this.buttoncursor.Name = "buttoncursor";
+            this.buttoncursor.Size = new System.Drawing.Size(97, 90);
+            this.buttoncursor.TabIndex = 4;
+            this.buttoncursor.Text = "imleci ustune getiriniz.";
+            this.buttoncursor.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(860, 466);
+            this.Controls.Add(this.buttoncursor);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttoncursor;
+    }
+}
+
---
+++ gorselodev1/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gorselodev1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            buttoncursor.Cursor = Cursors.Hand;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Point konum = new Point();
+
+            konum.X = 350;
+            konum.Y = 175;
+
+            Button btn = new Button();
+
+            btn.Location = konum;
+            btn.BackColor = Color.Red;
+            btn.Height = 50;
+
+            this.Controls.Add(btn);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Font = new Font("Snap ITC" , 25 , FontStyle.Bold);
+            label1.ForeColor = Color.Red;
+        }
+    }
+}
